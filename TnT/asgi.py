8316c7c7689549438d97397c56323b3d5a6ab982
Agrim Verma <agrimav000@gmail.com>
2022-05-02 01:56:49 +0530

    added explore page

--- TnT/asgi.py
+++ TnT/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tminus0 project.
+ASGI config for TourNTravel project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tminus0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TnT.settings')
 
 application = get_asgi_application()
--- TnT/wsgi.py
+++ TnT/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tminus0 project.
+WSGI config for TourNTravel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tminus0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TnT.settings')
 
 application = get_wsgi_application()
